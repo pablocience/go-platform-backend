00ef5cb3c0dde4812c09fefbe9cb608c87007fdb
PabloEnriquezCience <pablo.enriquez@cience.com>
2023-01-03 14:02:23 -0600

    feat: add job routes

--- src/routes/go-enrich.route.js
+++ src/routes/go-enrich.route.js
@@ -4,9 +4,12 @@ const {
   getMyFiles,
   uploadInputDataFile,
 } = require("../controllers/go-enrich.controller");
+const { runJob, getJobStatus } = require('../controllers/explorium.controller')
 
 // GET http://localhost:8080/api/go-enrich/myfiles
 router.get("/myfiles", getMyFiles);
 router.post("/myfiles/upload", uploadInputDataFile);
+router.post('/run-job/:recipeName', runJob)
+router.get('/query-job/:jobId')
 
 module.exports = router;
