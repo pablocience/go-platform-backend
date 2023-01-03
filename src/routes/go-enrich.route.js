0cafbfbf190f3a13c67952d8e08c9bfbb2de19e3
Pablo Zarate <pablo.zarate@cience.com>
2023-01-03 17:43:49 -0600

    feat: field mapping enpoints

--- src/routes/go-enrich.route.js
+++ src/routes/go-enrich.route.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const {
   getMyFiles,
   uploadInputDataFile,
+  getAvailableRecipes,
+  getAvailableRecipeColumns,
+  getAllAvailableRecipeColumns
 } = require("../controllers/go-enrich.controller");
-const { runJob, getJobStatus } = require('../controllers/explorium.controller')
+const { runJob, getJobStatus } = require("../controllers/explorium.controller");
 
 // GET http://localhost:8080/api/go-enrich/myfiles
 router.get("/myfiles", getMyFiles);
+router.get("/recipes", getAvailableRecipes);
+router.get("/recipes/:recipeId/columns", getAvailableRecipeColumns);
+router.get("/recipes/columns", getAllAvailableRecipeColumns);
 router.post("/myfiles/upload", uploadInputDataFile);
-router.post('/run-job/:recipeName', runJob)
-router.get('/query-job/:jobId', getJobStatus)
+router.post("/run-job/:recipeName", runJob);
+router.get("/query-job/:jobId", getJobStatus);
 
 module.exports = router;
